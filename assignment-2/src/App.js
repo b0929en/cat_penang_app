a1514eee3da898a0ba045bf7d2921aa898bc8b63
LawZheYin <lawzheyin2003@gmail.com>
2025-01-09 20:42:36 +0800

    Stage

--- assignment-2/src/App.js
+++ assignment-2/src/App.js
@@ -7,13 +7,12 @@ import './Styles/HotelCard.css'
 import React from 'react';
 import AnimatedStarting from './Components/AnimatedStarting';
 import ItemSection1 from './Sections/FoodNBeverages';
-import ItemSection2 from './Sections/Hotels'
-import Event from './Components/Event.jsx'
-import Footer from './Components/Footer.jsx'
-import Video from './Components/video.jsx'
+import Event from './Components/Event.jsx';
+import Footer from './Components/Footer.jsx';
+import Video from './Components/video.jsx';
 import ItemSection2 from './Sections/Hotels';
 import ItemSection3 from './Sections/TourismSpots';
-import ItemSection4 from './Sections/AboutUs'
+import ItemSection4 from './Sections/AboutUs';
 
 
 function App() {
