e611d00877702ad83ef96574bd48fd23d8f60ca8
LAPTOP-B09HT66U\Lenovo <benliew2016@gmail.com>
2025-01-09 21:19:32 +0800

    final update 1

---
+++ assignment-2/src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css';
+import './Styles/AnimatedStarting.css'
+import './Styles/FoodNBeverages.css'
+import './Styles/Hotels.css'
+import './Styles/CardTemplate.css'
+import './Styles/HotelCard.css'
+import React from 'react';
+import AnimatedStarting from './Components/AnimatedStarting.js';
+import ItemSection1 from './Sections/FoodNBeverages.js';
+import ItemSection2 from './Sections/Hotels.js'
+import Event from './Components/Event.jsx'
+import Footer from './Components/Footer.jsx'
+import Video from './Components/video.jsx'
+
+function App() {
+  return (
+    <>
+    <div>
+      <AnimatedStarting/>
+      <ItemSection1/>
+      <ItemSection2/>
+      <Event />
+      <Video />
+      <Footer />
+    </div>
+
+    </>
+  );
+}
+
+export default App;
+
